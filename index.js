f9de246aff072bbbf935877509af4bc56fa749d9
manighahremani <mashapicasso@gmail.com>
2022-07-06 02:58:21 +0100

    Check guesses and update word accordingly

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const words = [
 let guessed = [];
 // The word that the user needs to guess
 let word;
+// Stores all the needed DOM elements
+let el = {};
 
 /* 
 * Takes a number and returns a random index between 0 and that number
@@ -28,6 +30,25 @@ function randomElement(array) {
   return element;
 }
 
+/*
+* Checks if the letter is in the word and updates the guessed letters array
+* it also displays a message in the feedback section accordingly
+*/
+// TODO should only be called on unused words, also non alphabetical should be visible
+function checkLetter(letter) {
+  letter = letter.trim().toLowerCase();
+  let found = false;
+  for (let i = 0; i < word.length; i++) {
+    if (word[i].toLowerCase() === letter) {
+      guessed[i] = word[i];
+      found = true;
+    }
+  }
+
+  el.feedback.textContent = `${letter} is ${found ? '' : 'not'} in the word`;
+
+}
+
 /* 
 * Starts a new game by choosing a new word from the words array
 * and resetting the guessed letters array populating it with '_'s
@@ -41,8 +62,7 @@ function startNewGame() {
     guessed.push('_');
   }
 
-  const instruct = document.querySelector('#instruct');
-  instruct.textContent = guessed.join(' ');
+  el.instruct.textContent = guessed.join(' ');
 }
 
 /* 
@@ -50,21 +70,71 @@ function startNewGame() {
 */
 function drawKeyboard() {
   const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-  const keyboard = document.querySelector('#keyboard');
 
   for (const letter of alphabet) {
     const button = document.createElement('button');
     button.textContent = letter;
-    keyboard.append(button);
+    button.dataset.letter = letter;
+    el.keyboard.append(button);
   }
 }
 
+/*
+ * Respond to press of keys on the physical keyboard
+ */
+function checkKeyPress(e) {
+  console.log(e);
+  if (e.code.indexOf('Key') === 0) {
+    registerLetter(e.code[3]);
+  }
+}
+
+/*
+* Responds to the click of the on-screen keyboard
+*/
+function checkClick(e) {
+  const letter = e.target.dataset.letter;
+  if (letter) {
+    registerLetter(letter);
+  }
+}
+
+/*
+* Checks if the letter is in the word and updates the guessed letters array
+* it also updates the guessed word in the insntruct section
+*/
+function registerLetter(letter) {
+  checkLetter(letter);
+  el.instruct.textContent = guessed.join(' ');
+}
+
+/*
+* Adds event listeners to the on-screen keyboard and the physical keyboard
+*/
+function addEventListeners() {
+  window.addEventListener('keydown', checkKeyPress);
+  el.keyboard.addEventListener('click', checkClick);
+}
+
+/*
+* Selects needed DOM elements and stores them in global the el object
+*/
+function prepareHandles() {
+  el.keyboard = document.querySelector('#keyboard');
+  el.instruct = document.querySelector('#instruct');
+  el.feedback = document.querySelector('#feedback');
+}
+
 /* 
-* Initializes the game by starting a new game and drawing the keyboard
+* Initializes the game by first preparing handles to the DOM elements
+* then it starts a new game and draws the keyboard
+* it also adds an event listener to the on-screen and physical keyboard
 */
 function init() {
-  drawKeyboard();
+  prepareHandles();
   startNewGame();
+  drawKeyboard();
+  addEventListeners();
 }
 
 // The init function will be called when the page loads
