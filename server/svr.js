f0a2dba1a209f9d523bb4de7ac0c15749ec9a8f0
manighahrmani <mashapicasso@gmail.com>
2022-07-17 05:00:07 +0100

    Comments on svr.js

--- server/svr.js
+++ server/svr.js
@@ -1,8 +1,10 @@
-// To find out how to install express in your project visit: 
-// https://www.npmjs.com/package/express
+// To find out how to install express in your project: https://www.npmjs.com/package/express
+// More about serving static files with express: https://expressjs.com/en/starter/static-files.html
 import express from 'express';
 
 const app = express();
+// 'client' is the folder that contains the index.html and other static files
 app.use(express.static('client'));
 
+// 8080 is the default port for a server
 app.listen(8080);
