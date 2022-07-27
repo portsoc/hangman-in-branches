2a93d4ee6c2c8398047d1da822772a3b9c0c1ee7
manighahremani <mashapicasso@gmail.com>
2022-07-27 03:51:06 +0100

    Remove old commits

--- server/svr.js
+++ server/svr.js
@@ -1,12 +1,8 @@
-// To find out how to install express in your project: https://www.npmjs.com/package/express
-// More about serving static files with express: https://expressjs.com/en/starter/static-files.html
 import express from 'express';
 
 const app = express();
-// 'client' is the folder that contains the index.html and other static files
 app.use(express.static('client'));
 
-// 8080 is the default port for a server
 app.listen(8080);
 
 console.log('Server is running on port 8080. View it by visiting the following link in the browser:');
