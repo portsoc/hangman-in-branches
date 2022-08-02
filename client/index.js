56b2bc7cf72d15ce08c9d2d708818dba3deab5e8
mani ghahremani <mashapicasso@gmail.com>
2022-08-02 18:27:21 +0100

    Only update keyboard if it exists

--- client/index.js
+++ client/index.js
@@ -240,11 +240,15 @@ function redrawWord() {
  */
 function redrawKeyboard() {
   const keyboard = document.querySelector('#keyboard');
-  const keyboardLetters = keyboard.querySelectorAll('[data-letter]');
-  for (const letter of keyboardLetters) {
-    const hitsAndMisses = hits.concat(misses);
-    if (hitsAndMisses.includes(letter.dataset.letter)) {
-      letter.disabled = true;
+  
+  // only update if the keyboard exists (on game over it gets deleted)
+  if (keyboard) {
+    const keyboardLetters = keyboard.querySelectorAll('[data-letter]');
+    for (const letter of keyboardLetters) {
+      const hitsAndMisses = gameState.hits.concat(gameState.misses);
+      if (hitsAndMisses.includes(letter.dataset.letter)) {
+        letter.disabled = true;
+      }
     }
   }
 }
