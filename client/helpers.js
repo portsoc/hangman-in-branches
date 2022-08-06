4f3a5dce6799cb6bb1307c35d8eaf64093ea30bd
manighahremani <mashapicasso@gmail.com>
2022-08-06 18:31:50 +0100

    Minor comment updatee

--- client/helpers.js
+++ client/helpers.js
@@ -36,8 +36,8 @@ export function create(type, parent, attributes = {}, text) {
 }
 
 /**
- * Draws the keyboard on the screen by creating a button for each letter
- * @param parent - The parent element to append the keyboard to.
+ * Draws the keyboard on the screen by creating a button for each letter.
+ * @param parent - The parent element to append the keyboard to
  * @returns The keyboard element
  */
 export function drawKeyboard(parent) {
