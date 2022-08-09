b3bb363327cee6bfd16a2f71c6f733b179e8a85f
manighahrmani <mashapicasso@gmail.com>
2022-08-09 23:55:57 +0100

    Midway through creategame

--- server/svr.js
+++ server/svr.js
@@ -13,6 +13,14 @@ const words = [
   'The Lord of the Rings', 'The Dark Knight', 'Pulp Fiction',
 ];
 
+/**
+ * Stores the status of the game and has the following properties:
+ * `word` - the word to be guessed,
+ * `hits` - an array of the letters that have been guessed correctly,
+ * `misses` - an array of the letters that have been guessed incorrectly,
+ * `onGoing` - a boolean that indicates if the game is still in progress,
+ * `userWord` - an array of letters that has been guessed so far('_' for unguessed letters).
+ */
 const status = {};
 
 /**
@@ -39,12 +47,9 @@ function randomElement(array) {
 }
 
 /**
- * Creates a new game by resettting the properties of `status` variable:
- * `word` - the word to be guessed,
- * `hits` - an array of the letters that have been guessed correctly,
- * `misses` - an array of the letters that have been guessed incorrectly,
- * `onGoing` - a boolean that indicates if the game is still in progress,
- * `userWord` - an array of letters that has been guessed so far ('_' for unguessed letters).
+ * Creates a new game by resettting the properties of `status` variable.
+ * @param req - the request object
+ * @param res - response object, contains the sanitized `status` variable (without `word` property)
  */
 function createGame(req, res) {
   status.word = randomElement(words);
@@ -54,14 +59,74 @@ function createGame(req, res) {
   status.userWord = status.word.replace(/[a-z]/ig, '_').split('');
 
   // use the spread operator to copy the object otherwise it will be a reference to the same object
+  // for more info visit the following link:
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
   const sanitizedStatus = { ...status };
   delete sanitizedStatus.word;
   res.json(sanitizedStatus);
+}
+
+/**
+ * Checks if a given letter is in the word, then updates `gameState.userWord`.
+ * @param letter - the letter that the user guessed
+ * @returns `true` if the letter is in the word, `false` otherwise
+ */
+function checkLetter(letter) {
+  let found = false;
+  const lowercaseWord = game.word.toLowerCase();
+
+  for (let i = 0; i < gameState.word.length; i++) {
+    if (lowercaseWord[i] === letter.toLowerCase()) {
+      gameState.userWord[i] = gameState.word[i];
+      found = true;
+    }
+  }
+
+  return found;
+}
+
+/**
+ * Checks whether the user has guessed the word.
+ * @returns `true` if the user has guessed the word, `false` otherwise
+ */
+function checkWon() {
+  return gameState.userWord.join('') === gameState.word;
+}
+
+/**
+ * It returns a copy of the status object, but without the word property
+ * @returns the status object with the word property deleted
+ */
+function sanitizedStatus() {
+  const result = { ...status };
+  delete result.word;
+  return result;
+}
+
+function guessLetter(req, res) {
+  if (game.ongoing) {
+    const hit = checkLetter(req.params.letter);
+
+    if (hit) {
+      game.hits.push(letter);
+    } else {
+      game.misses.push(letter);
+    }
+
+    const won = checkWon();
+    if (won || game.misses.length > 9) {
+      game.ongoing = false;
+    }
+  }
+
+  return game.ongoing ? sanitizedStatus(id) : game;
+  res.json(game.guessLetter(req.params.id, req.params.letter));
 }
 
 app.post('/games/', createGame);
 
 app.listen(8080);
 
-console.log('Server is running on port 8080. View it by visiting the following link in the browser:');
+console.log('Server is running on port 8080. \
+View it by visiting the following link in the browser: ');
 console.log('http://localhost:8080/');
