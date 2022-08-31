dd3bcfca98102e50ca9ae032055e826d4c6d4625
manighahrmani <mashapicasso@gmail.com>
2022-08-31 01:10:30 +0100

    Update sql to scenario

--- server/game.sql
+++ server/game.sql
@@ -1,16 +1,20 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-DROP TABLE IF EXISTS Messageboard;
+DROP TABLE IF EXISTS game;
 
-CREATE TABLE IF NOT EXISTS Messageboard (
-    id   uuid      DEFAULT uuid_generate_v4() PRIMARY KEY,
-    time timestamp DEFAULT now(),
-    msg  text
+CREATE TABLE IF NOT EXISTS game (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    word VARCHAR(255) NOT NULL,
+    hits VARCHAR(255) NOT NULL,
+    misses VARCHAR(255) NOT NULL,
+    ongoing BOOLEAN NOT NULL DEFAULT true,
+    userWord VARCHAR(255) NOT NULL,
+    last BOOLEAN NOT NULL DEFAULT false,
+    won BOOLEAN NOT NULL DEFAULT false,
 );
 
-INSERT INTO Messageboard (msg) VALUES
-( 'these are three default messages' ),
-( 'delivered from the server' ),
-( 'using a custom route' );
+INSERT INTO game (word, hits, misses, ongoing, userWord, last, won) VALUES ('test', '', '', true, '', false, false);
 
-SELECt * FROM Messageboard;
+SELECT * FROM game;
+
+DROP TABLE IF EXISTS Messageboard;
