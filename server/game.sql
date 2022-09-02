7e423087ab79eec40e6e3a4838227dcc515dde27
manighahrmani <mashapicasso@gmail.com>
2022-09-02 16:54:40 +0100

    ongoing to camelCase

--- server/game.sql
+++ server/game.sql
@@ -7,12 +7,12 @@ CREATE TABLE IF NOT EXISTS game (
     word VARCHAR(255) NOT NULL,
     hits VARCHAR(255) NOT NULL,
     misses VARCHAR(255) NOT NULL,
-    ongoing BOOLEAN NOT NULL DEFAULT true,
+    onGoing BOOLEAN NOT NULL DEFAULT true,
     userWord VARCHAR(255) NOT NULL,
     last BOOLEAN NOT NULL DEFAULT false,
     won BOOLEAN NOT NULL DEFAULT false
 );
 
-INSERT INTO game (word, hits, misses, ongoing, userWord, last, won) VALUES ('test', '', '', true, '', false, false);
+INSERT INTO game (word, hits, misses, onGoing, userWord, last, won) VALUES ('test', '', '', true, '', false, false);
 
 SELECT * FROM game;
