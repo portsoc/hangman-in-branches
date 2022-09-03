65d4f4dbb0d3dfbbf6918cd3dc35e68f35389f60
manighahrmani <mashapicasso@gmail.com>
2022-09-03 21:17:47 +0100

    Leave vm config line on

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 export default {
   // leave this line uncommented if running on our VM)
-  // host: '/var/run/postgresql',
+  host: '/var/run/postgresql',
 
   database: 'hangmanDB',
   statement_timeout: 5000,
