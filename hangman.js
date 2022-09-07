81969489b4db0122e2e2d281aede1296fe22d2c6
manighahrmani <mashapicasso@gmail.com>
2022-09-07 17:58:15 +0100

    Fix indentation

--- hangman.js
+++ hangman.js
@@ -1,7 +1,7 @@
 const words = [
-    'Jurassic Park', 'Star Wars', 'The Matrix',
-    'The Godfather', 'The Dark Knight', 'The Lord of the Rings',
-    'The Lord of the Rings', 'The Dark Knight', 'Pulp Fiction',
+  'Jurassic Park', 'Star Wars', 'The Matrix',
+  'The Godfather', 'The Dark Knight', 'The Lord of the Rings',
+  'The Lord of the Rings', 'The Dark Knight', 'Pulp Fiction',
 ];
 
 /* 
@@ -9,18 +9,18 @@ const words = [
 * The number itself is not included in the range
 */
 function randomIndex(size) {
-    const index = Math.floor(Math.random() * size);
-    return index;
+  const index = Math.floor(Math.random() * size);
+  return index;
 }
 
 /*
 * Takes a an array and returns a random element from that array
 */
 function randomElement(array) {
-    const size = array.length;
-    const index = randomIndex(size);
-    const element = array[index];
-    return element;
+  const size = array.length;
+  const index = randomIndex(size);
+  const element = array[index];
+  return element;
 }
 
 let word = randomElement(words);
