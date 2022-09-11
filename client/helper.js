9bf5e83f7771521d8a83e35642929c57e30fc12a
manighahrmani <mashapicasso@gmail.com>
2022-09-11 18:34:41 +0100

    Update helper.js in client

--- client/helper.js
+++ client/helper.js
@@ -1,5 +1,5 @@
 /**
- * Remove all elements that match the given selector.
+ * Removes all elements that match `selector`
  * @param selector - A string that specifies the CSS selector of the elements to remove
  */
 export function safeRemove(selector) {
@@ -11,8 +11,8 @@ export function safeRemove(selector) {
 }
 
 /**
- * It creates an element of the given type, sets the attributes and text content (if
- * provided), and appends the element to the parent element (if provided).
+ * Creates an element of given `type`, sets `attributes` and `text`
+ * and appends the element to `parent`.
  * @param type - the type of the element to be created, e.g. div, p, h1, etc.
  * @param parent - the parent element to append the new element to
  * @param [attributes] - a map of attributes to be set on the element
@@ -36,29 +36,22 @@ export function create(type, parent, attributes = {}, text) {
 }
 
 /**
- * Draws the keyboard on the screen by creating a button for each letter.
- * @param parent - The parent element to append the keyboard to
- * @returns The keyboard element
+ * Draws the keys on the keyboard
+ * @param keyboard - The keyboard element to append the keys to
  */
-export function drawKeyboard(parent) {
-  // separate the letters into 3 rows similar to a QWERTY keyboard
+export function drawKeyboard(keyboard) {
   const alphabet = ['qwertyuiop', 'asdfghjkl', 'zxcvbnm'];
-
-  const keyboard = create('section', parent, { id: 'keyboard' });
 
   for (const row of alphabet) {
     const letterRow = create('div', keyboard, { id: 'keys_' + row });
-    // rows have unique ids but share the same class so we can style them all the same way
     letterRow.classList.add('letterRow');
 
     for (const letter of row) {
       const btn = create('button', letterRow, {
         id: 'key_' + letter,
-        textContent: letter,
+        textContent: letter
       }, letter);
       btn.dataset.letter = letter;
     }
   }
-
-  return keyboard;
 }
